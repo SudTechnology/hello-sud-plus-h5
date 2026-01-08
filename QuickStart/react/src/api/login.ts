f6addc97d9f20c510c9c61cca100a72a7c883275
lwcReber <1079670310@qq.com>
2026-01-08 18:23:33 +0800

    feat：修改getcode接口

--- QuickStart/react/src/api/login.ts
+++ QuickStart/react/src/api/login.ts
@@ -3,7 +3,7 @@ import request from 'utils/request'
 // 游戏列表
 export const getCode = (data: any) => {
   return request({
-    url: `https://mgp-hello.sudden.ltd/login/v3`,
+    url: 'https://prod-hellosud-base.s00.tech/login/v3', // 业务方自行实现该接口
     method: 'post',
     data
   })
--- QuickStart/vue/src/api/login.js
+++ QuickStart/vue/src/api/login.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 游戏列表
 export const getCode = (data) => {
   return request({
-    url: 'https://mgp-hello.sudden.ltd/login/v3', // 业务方自行实现该接口
+    url: 'https://prod-hellosud-base.s00.tech/login/v3', // 业务方自行实现该接口
     method: 'post',
     data
   })
